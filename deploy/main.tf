344851039e304bebcee0b875275883a010947e67
Daniel Ayuga <danielayugachacon@outlook.com>
2025-06-02 22:06:11 +0200

    Update main.tf

--- deploy/main.tf
+++ deploy/main.tf
@@ -39,6 +39,63 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+# Reglas de NSG
+resource "azurerm_network_security_rule" "allow_bastion_rdp" {
+  resource_group_name         = azurerm_resource_group.rg.name
+  name                        = "Allow-Bastion-RDP"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "AzureBastionSubnet"
+  destination_address_prefix  = "VirtualNetwork"
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "deny_all_inbound" {
+  resource_group_name         = azurerm_resource_group.rg.name
+  name                        = "Deny-All-Inbound"
+  priority                    = 200
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "allow_storage_access" {
+  resource_group_name         = azurerm_resource_group.rg.name
+  name                        = "Allow-Storage-Access"
+  priority                    = 300
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "VirtualNetwork"
+  destination_address_prefix  = "Storage"
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "deny_all_outbound" {
+  resource_group_name         = azurerm_resource_group.rg.name
+  name                        = "Deny-All-Outbound"
+  priority                    = 400
+  direction                   = "Outbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
 # Interfaz de red
 resource "azurerm_network_interface" "nic-ad-ds" {
   name                = "nic-ad-ds"
