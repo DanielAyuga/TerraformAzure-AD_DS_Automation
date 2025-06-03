05a44d39740b55cab65c8498d7d9e63dfc39064e
Daniel Ayuga <danielayugachacon@outlook.com>
2025-06-03 15:53:20 +0200

    Update ad_setup.ps1

--- deploy/ad_setup.ps1
+++ deploy/ad_setup.ps1
@@ -4,20 +4,11 @@ Install-WindowsFeature RSAT-AD-PowerShell
 Import-Module ActiveDirectory
 Install-WindowsFeature -Name DNS -IncludeManagementTools
 
+#Tarea programada para ejecutar otro script tras el reinicio de instalación del ADDSForest
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File C:\post_ad_setup.ps1"
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
+Register-ScheduledTask -TaskName "PostADDSConfig" -Action $action -Trigger $trigger -Principal $principal
+
 # Creación del bosque y dominio en Active Directory (con un dominio interno)
 Install-ADDSForest -DomainName "miejemplo.local" -ForestMode WinThreshold -DomainMode WinThreshold -InstallDNS -SafeModeAdministratorPassword (ConvertTo-SecureString "P@ssw0rd1" -AsPlainText -Force) -Confirm:$false
-
-# Agregar el sufijo UPN para sincronización con Entra ID
-$context = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
-$context.UpnSuffixes.Add("danielayugachaconoutlook.onmicrosoft.com")
-Write-Output "✔️ Sufijo UPN agregado correctamente."
-
-# Configuración de políticas de grupo (GPO) en SYSVOL
-$gpoPath = "C:\\Windows\\SYSVOL\\domain\\Policies"
-New-Item -Path "$gpoPath\\GPO-Seguridad" -ItemType Directory
-New-Item -Path "$gpoPath\\GPO-Restricciones" -ItemType Directory
-    
-Import-Module GroupPolicy
-New-GPO -Name "BloquearUSB"
-New-GPLink -Name "BloquearUSB" -Target "DC=miejemplo,DC=local"
-Set-GPRegistryValue -Name "BloquearUSB" -Key "HKLM\\SYSTEM\\CurrentControlSet\\Services\\USBSTOR" -ValueName "Start" -Type DWORD -Value 4
