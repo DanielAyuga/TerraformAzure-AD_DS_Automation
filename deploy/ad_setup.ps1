b952edeed1fc8faa6f2644a33e8c4c13adcbf45f
Daniel Ayuga <danielayugachacon@outlook.com>
2025-06-05 20:04:20 +0200

    Update ad_setup.ps1

--- deploy/ad_setup.ps1
+++ deploy/ad_setup.ps1
@@ -6,9 +6,9 @@ Install-WindowsFeature -Name DNS -IncludeManagementTools
 
 #Tarea programada para ejecutar otro script tras el reinicio de instalación del ADDSForest
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File C:\post_ad_setup.ps1"
-$trigger = New-ScheduledTaskTrigger -AtStartup
+$trigger = New-ScheduledTaskTrigger -AtLogon
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
 Register-ScheduledTask -TaskName "PostADDSConfig" -Action $action -Trigger $trigger -Principal $principal
 
-# Creación del bosque y dominio en Active Directory (con un dominio interno)
+# Creación del bosque y dominio en Active Directory con un dominio interno
 Install-ADDSForest -DomainName "miejemplo.local" -ForestMode WinThreshold -DomainMode WinThreshold -InstallDNS -SafeModeAdministratorPassword (ConvertTo-SecureString "P@ssw0rd1" -AsPlainText -Force) -Confirm:$false
