e2e153654424f190d49a5342d2b68b6d74f7cff9
Daniel Ayuga <danielayugachacon@outlook.com>
2025-06-05 20:02:58 +0200

    Update main.tf

--- deploy/main.tf
+++ deploy/main.tf
@@ -26,14 +26,6 @@ resource "azurerm_subnet" "subnet" {
   service_endpoints = ["Microsoft.Storage"]
 }
 
-# Subred para Azure Bastion
-resource "azurerm_subnet" "bastion_subnet" {
-  name                 = "AzureBastionSubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/27"]
-}
-
 # Grupo de seguridad de red
 resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-ad-ds"
@@ -51,7 +43,7 @@ resource "azurerm_network_security_rule" "allow_bastion_rdp" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "3389"
-  source_address_prefix       = "AzureBastionSubnet"
+  source_address_prefixes       = ["10.0.2.0/27"]
   destination_address_prefix  = "VirtualNetwork"
   network_security_group_name = azurerm_network_security_group.nsg.name
 }
@@ -98,6 +90,36 @@ resource "azurerm_network_security_rule" "deny_all_outbound" {
   network_security_group_name = azurerm_network_security_group.nsg.name
 }
 
+# Azure Bastion
+resource "azurerm_bastion_host" "bastion" {
+  name                = "bastion-ad-ds"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                 = "bastion-ip-config"
+    subnet_id            = azurerm_subnet.bastion_subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion_ip.id
+  }
+}
+
+# Subred para Azure Bastion
+resource "azurerm_subnet" "bastion_subnet" {
+  name                 = "AzureBastionSubnet"  # Nombre obligatorio
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/27"]  # Recomendado para Bastion
+}
+
+# IP pública para Azure Bastión
+resource "azurerm_public_ip" "bastion_ip" {
+  name                = "bastion-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"  # Bastion requiere IP Standard
+}
+
 # Interfaz de red
 resource "azurerm_network_interface" "nic-ad-ds" {
   name                = "nic-ad-ds"
@@ -133,27 +155,23 @@ resource "azurerm_windows_virtual_machine" "ad-ds-vm" {
     sku       = "2019-Datacenter"
     version   = "latest"
   }
-}
 
-# Azure Bastion
-resource "azurerm_bastion_host" "bastion" {
-  name                = "bastion-ad-ds"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                 = "bastion-ip-config"
-    subnet_id            = azurerm_subnet.bastion_subnet.id
-    public_ip_address_id = azurerm_public_ip.bastion_ip.id
+  identity {
+    type = "SystemAssigned"  # Managed Identity activada
   }
 }
 
-resource "azurerm_public_ip" "bastion_ip" {
-  name                = "bastion-public-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+# Creación de la extensión "CustomScriptExtension"
+resource "azurerm_virtual_machine_extension" "run_ad_setup" {
+  name                 = "run-ad-setup"
+  virtual_machine_id   = azurerm_windows_virtual_machine.ad-ds-vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.10"
+
+  settings = jsonencode({
+    commandToExecute = "powershell -ExecutionPolicy Unrestricted -Command \"Invoke-WebRequest -Uri 'https://${azurerm_storage_account.storage.name}.blob.core.windows.net/${azurerm_storage_container.scripts_container.name}/ad_setup.ps1${data.azurerm_storage_account_sas.storagesas.sas}' -OutFile 'C:\\ad_setup.ps1'; Invoke-WebRequest -Uri 'https://${azurerm_storage_account.storage.name}.blob.core.windows.net/${azurerm_storage_container.scripts_container.name}/post_ad_setup.ps1${data.azurerm_storage_account_sas.storagesas.sas}' -OutFile 'C:\\post_ad_setup.ps1'; & 'C:\\ad_setup.ps1'\""
+  })
 }
 
 # Cuenta de almacenamiento
@@ -165,13 +183,13 @@ resource "azurerm_storage_account" "storage" {
   account_replication_type = "LRS"
 
   network_rules {
-    default_action             = "Deny" 
-    virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
-    bypass                     = ["AzureServices"]
+    default_action             = "Allow" # Bloquea accesos desde Internet
+    virtual_network_subnet_ids = [azurerm_subnet.subnet.id] # Permite solo acceso desde la VNet
+    bypass                     = ["AzureServices"] # Permite acceso desde servicios internos de Azure
   }
 }
 
-# Contenedor para la cuenta de almacenamiento
+# Creación de contenedor
 resource "azurerm_storage_container" "scripts_container" {
   name                  = "scripts"
   storage_account_id    = azurerm_storage_account.storage.id
@@ -196,30 +214,33 @@ resource "azurerm_storage_blob" "post_ad_setup_script" {
   source                 = var.ruta_local_post_ad_setup
 }
 
-# Creación del blob dentro del contenedor
-resource "azurerm_storage_blob" "users_json" {
-  name                   = "users_json"
-  storage_account_name   = azurerm_storage_account.storage.name
-  storage_container_name = azurerm_storage_container.scripts_container.name
-  type                   = "Block"
-  source                 = var.ruta_local_users_json
+resource "azurerm_key_vault" "keyvault" {
+  name                = "mykvdsfs64565dfsrhs"  # Debe ser único globalmente
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = var.tenant_id
+  sku_name            = "standard"
+  enable_rbac_authorization = true
 }
 
-# Extensión de vm
-resource "azurerm_virtual_machine_extension" "run_ad_setup" {
-  name                 = "run-ad-setup"
-  virtual_machine_id   = azurerm_windows_virtual_machine.ad-ds-vm.id
-  publisher            = "Microsoft.Compute"
-  type                 = "CustomScriptExtension"
-  type_handler_version = "1.10"
+resource "azurerm_role_assignment" "kv_secrets_admin" {
+  scope                = azurerm_key_vault.keyvault.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
 
-  settings = <<SETTINGS                                                         
-    {
-      "fileUris": [
-        "https://${azurerm_storage_account.storage.name}.blob.core.windows.net/${azurerm_storage_container.scripts_container.name}/ad_setup.ps1",
-        "https://${azurerm_storage_account.storage.name}.blob.core.windows.net/${azurerm_storage_container.scripts_container.name}/post_ad_setup.ps1"
-      ],
-      "commandToExecute": "powershell -ExecutionPolicy Unrestricted -Command \"Invoke-WebRequest -Uri 'https://${azurerm_storage_account.storage.name}.blob.core.windows.net/${azurerm_storage_container.scripts_container.name}/ad_setup.ps1' -OutFile 'C:\\ad_setup.ps1'; Invoke-WebRequest -Uri 'https://${azurerm_storage_account.storage.name}.blob.core.windows.net/${azurerm_storage_container.scripts_container.name}/post_ad_setup.ps1' -OutFile 'C:\\post_ad_setup.ps1'; powershell -ExecutionPolicy Unrestricted -File C:\\ad_setup.ps1\""
-    }
-SETTINGS
+resource "azurerm_key_vault_secret" "sas_token" {
+  name         = "blob-sas-token"
+  value        = data.azurerm_storage_account_sas.storagesas.sas
+  key_vault_id = azurerm_key_vault.keyvault.id
+
+  depends_on = [
+    azurerm_role_assignment.kv_secrets_admin
+  ]
+}
+
+resource "azurerm_role_assignment" "kv_secrets_user" {
+  scope                = azurerm_key_vault.keyvault.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_windows_virtual_machine.ad-ds-vm.identity[0].principal_id
 }
