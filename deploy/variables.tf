2338f4f1ac902b76d699bd420b37aeb273110684
Daniel Ayuga <danielayugachacon@outlook.com>
2025-06-06 10:37:52 +0200

    Update variables.tf

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -1,24 +1,22 @@
 variable "tenant_id" {
   type = string
-  sensitive = true  # Oculta la id en logs
+  sensitive = true
 }
 
 variable "subscription_id" {
   type = string
-  sensitive = true  # Oculta la id en logs
+  sensitive = true
 }
 
 variable "admin_password" {
   type      = string
-  sensitive = true  # Oculta la contraseña en logs
+  sensitive = true
 }
 
 variable "ruta_local_ad_setup" {
   type      = string
-  sensitive = true  # Oculta la contraseña en logs
 }
 
 variable "ruta_local_post_ad_setup" {
   type      = string
-  sensitive = true  # Oculta la contraseña en logs
 }
